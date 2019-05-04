8296f5f576575fa9f7a9f5dd8d888890c2156de0
Todd Bluhm <toddbluhm@gmail.com>
2019-05-04 16:24:28 -0500

    Update examples to support new env-cmd v9
    - Added in better naming clarification of examples being executed when
    running `npm test`
    - Added in support for the new `--use-shell` option
    - Updated `env-cmd` to v9
    - Updated this repo version number to track with `env-cmd` version
    numbering

--- file-example/index.js
+++ file-example/index.js
@@ -2,8 +2,10 @@ const assert = require('assert')
 
 console.log(`Environment ${process.env.NODE_ENV}`)
 console.log(`ENVVAR: ${process.env.ENVVAR}`)
+console.log(`ENV_PATH: ${process.env.ENV_PATH}`)
 
 assert(process.env.NODE_ENV === 'env-file')
 assert(process.env.ENVVAR === 'exists')
+assert(!!~['./.env', './.env-file'].indexOf(process.env.ENV_PATH))
 
 console.log('Asserts Pass!')
