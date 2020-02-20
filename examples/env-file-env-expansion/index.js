519e1b621c0607e944a688e1bc53b7251f7a199f
Todd Bluhm <toddbluhm@gmail.com>
2020-02-20 03:12:44 -0600

    test: fix argument lookup location for nix systems

--- examples/env-file-env-expansion/index.js
+++ examples/env-file-env-expansion/index.js
@@ -10,13 +10,14 @@ console.log(`ENV_PATH: ${process.env.ENV_PATH}`)
 assert(process.env.NODE_ENV === 'env-file')
 assert(process.env.ENVVAR === 'exists')
 assert(process.env.ENV_PATH === './.env')
-const option = process.argv[process.argv.length - 1]
 if (process.platform === 'win32') {
+  const option = process.argv[process.argv.length - 1]
   if (option.includes('--pathWin32')) {
     assert(option.split('=')[1] === './.env')
   }
 }
 else {
+  const option = process.argv[process.argv.length - 2]
   if (option.includes('--path')) {
     assert(option.split('=')[1] === './.env')
   }
